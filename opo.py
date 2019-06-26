58fd48773d6c247689b07c76ef41df1d3a95c097
Vladislav <49246934+Huzzky@users.noreply.github.com>
2019-06-27 01:10:07 +0300

    Update opo.py

--- opo.py
+++ opo.py
@@ -96,11 +96,7 @@ def OMl(message):
 						pass
 				#print(description)
 				bot.send_message(message.chat.id, '\n\n'.join(description))
-				description.remove(description[0])
-				description.remove(description[1])
-				description.remove(description[2])
-				description.remove(description[3])
-				description.remove(description[4])
+				description = []
 				msg = bot.send_message(message.chat.id, "Ты можешь выбрать 'Выход' или написать название кинотетра. Например: 'ТРЦ «Миля»'. \n\nЕсли не знаешь какие есть кинотетры, то напиши 'Список кинотеатров'.\nЕсли что-то не получается, то обратись к команде /help. \nВыбирай.")
 				bot.register_next_step_handler(msg, seans_cinema)
 			except IndexError:
