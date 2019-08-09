aea24bcd847ccd79dcc029b6141c81a78cf861cf
Vladislav <49246934+Huzzky@users.noreply.github.com>
2019-08-09 22:15:48 +0300

    Update opo.py

--- opo.py
+++ opo.py
@@ -185,10 +185,10 @@ fa,fo ='',''
 
 @bot.message_handler(commands=['kino'])
 def first_step(message):
+	global count, fa, fo,count2,b,a,c
 	a = []
 	fa,fo,count2 = '','',1
 	b,c=[],[]
-	global count, fa, fo,count2,b,a,c
 	data1 = requests.get('https://msk.kinoafisha.info/cinema/')
 	data = data1.text 
 	bs = BeautifulSoup(data, 'html.parser')
